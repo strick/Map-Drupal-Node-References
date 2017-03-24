a1d58755d6a404b5074b8f8ba745c5d1a8b9144e
Brian Strickland <bstrick@gmail.com>
2017-03-24 16:17:12 -0400

    Make the controller more generic

--- src/Controller/MapAuthorsToQuotesController.php
+++ src/Controller/MapAuthorsToQuotesController.php
@@ -3,41 +3,42 @@
  * @file
  * Contains \Drupal\hello_world\Controller\HelloController.
  */
-namespace Drupal\map_authors_to_quotes\Controller;
+namespace Drupal\map_drupal_node_reference\Controller;
 
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
-class MapAuthorsToQuotesController {
-  public function content() {
-	$markup = "";
+class MapDrupalNodeReferencesController {
+	
+	// TODO: Make this accept start and end counts to proccess.
+	public function authorsToQuotes() {
+		
+		$markup = "";
 
-	// Switch to external database
-	\Drupal\Core\Database\Database::setActiveConnection('gtt6');
+		// Switch to external database
+		\Drupal\Core\Database\Database::setActiveConnection('gtt6');
 
-	// Get a connection going
-	$db = \Drupal\Core\Database\Database::getConnection();
+		// Get a connection going
+		$db = \Drupal\Core\Database\Database::getConnection();
 
-	$query = $db->select('content_type_quote', 'ctq');
-	$query->fields('ctq', array('nid', 'field_author_nid'));
-	//$query->range(1,10);
-	$quotes = $query->execute()->fetchAll();
+		$query = $db->select('content_type_quote', 'ctq');
+		$query->fields('ctq', array('nid', 'field_author_nid'));
+		//$query->range(1,10);
+		$quotes = $query->execute()->fetchAll();
 
-$coutn = 0;
-	// Switch back
-	\Drupal\Core\Database\Database::setActiveConnection();
-	foreach($quotes as $quote){
-$node = \Drupal::entityTypeManager()->getStorage('node')->load($quote->nid);
-
-	       // $node = \Drupal\Core\Entity\Entity::load(723798);//$quote->nid);
-        	$node->field_author->target_id = $quote->field_author_nid;
-        	$node->save();
-       // 	$markup .= 'Updated quote ' . $quote->nid . '<br />';
-$count++;
-	}
-    return array(
-      '#type' => 'markup',
-      '#markup' => t($count),
-    );
-  }
+		$count = 0;
+		// Switch back
+		\Drupal\Core\Database\Database::setActiveConnection();
+		foreach($quotes as $quote){
+			$node = \Drupal::entityTypeManager()->getStorage('node')->load($quote->nid);
+			$node->field_author->target_id = $quote->field_author_nid;
+			$node->save();
+			$count++;
+		}
+		
+	    	return array(
+	      		'#type' => 'markup',
+	      		'#markup' => t($count),
+	    	);
+  	}
 }
